fda45d33798922508d3322ba70568f485addc12d
beevo <vo.bee92@gmail.com>
2020-01-29 12:17:46 -0600

    Updates

---
+++ precache-manifest.e654b4e5f3d8a3eab774f593618798ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d973c72621d49aff8ce001244aee2a",
+    "url": "/simple-form/index.html"
+  },
+  {
+    "revision": "920dd85cd0d6825032af",
+    "url": "/simple-form/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "831c28d44e3612c681a6",
+    "url": "/simple-form/static/js/2.05fdd6b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-form/static/js/2.05fdd6b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "920dd85cd0d6825032af",
+    "url": "/simple-form/static/js/main.47e16357.chunk.js"
+  },
+  {
+    "revision": "9ca05adbcef5c54e5b40",
+    "url": "/simple-form/static/js/runtime-main.9d830e69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-form/static/media/logo.5d5d9eef.svg"
+  }
+]);
